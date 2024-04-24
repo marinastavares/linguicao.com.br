319c1a676327e6f6634140b1e9632d22e2ca8124
victorsouza89 <69157327+victorsouza89@users.noreply.github.com>
2024-04-24 02:37:03 -0300

    add vendedores 39

--- generateVendedores.js
+++ generateVendedores.js
@@ -1,126 +1,187 @@
-const names = [
-  "Artur Rabelo",
-  "Otávio Augusto",
-  "Alex Lago",
-  "Gregory Selau",
-  "Vitor Sagás",
-  "Breno Moreira",
-  "Giulia Cenci",
-  "Gustavo Gonçalves",
-  "Pedro Sachs",
-  "Paulo Librelotto",
-  "Isaque Luís",
-  "Leonardo Godoy",
-  "Maria Eduarda",
-  "Brenda Eduarda",
-  "Fernanda Pilz",
-  "Ana Cecilia Boeng",
-  "Luiz Felipe Dourado",
-  "Michelle Amorim",
-  "Rafael Antônio",
-  "Luan Lourenço",
-  "Mariana Nunes",
-  "Nathalia da Silva",
-  "Vitória Menoncin",
-  "Ana Beatriz",
-  "Julia Aleksandra",
-  "Clara Cardoso",
-  "Irys Schlickmann",
-];
-const drive_images_ids = [
-  "1Y9ah_aEXNs0cDVMYeBuIh4027m6F9QMC",
-  "1YFj2ZkatiR2qycQAcsk0n90lFhDLiCmI",
-  "1fiShOEJGZFAQZl2SLgk3CdwbntFQYnwl",
-  "1NKJZrWqDM_Gx-XIIuHi1ML1w25gMoyOi",
-  "1xv8Z-9bwu0n26x9tnB_ur-xu19450oU6",
-  "1_4tO-uQoT4vcix2InIDzUujeMDAKt_hI",
-  "1Xf2x6eYOqyUhwLiAU-B3rFanlAWIAT1E",
-  "1pNwR6X9bsiU64aa5eodKoIZiCruv9E1u",
-  "1GiDReIFGyVuK53kBgxTfrRF94Cy65rIN",
-  "1Pjn0GcBxKFC7awdmbyR4B7tx9Gl0_RtU",
-  "1keUKpZNC2DQt7UiBEabjDUKu6Nx045BE",
-  "1pVU0X-326qgERzZbJtXBGajRfJ6HuHri",
-  "1oM76I9mcfRbZ-pp_wynQhqTf6IOpZQqo",
-  "1SB1os1ZfUqkKHEXeImqb_PDvJUqGtu10",
-  "1PCENvg1HuJN5b_JhBl5Pq6Ur9lSHp513",
-  "1YccfATuKHxkibOj-L2nzfsUIS7ZC_z9v",
-  "1t5k_kqduhu2lgjAdfuhDpsbnINMJojHx",
-  "1xX1j0NnU-TBfaoMuAc6hyXQKCFTX9Bs3",
-  "1GNrWq5YrRuzaaEWRUJLi3MlrZnsUxaLD",
-  "1H2qid-F51gTA98NpX-jwizs_OOqhBk6P",
-  "102WZ_Wu4QMnyaI_xX6B8ePGm8aiBwckV",
-  "1MThFScJC72X7PPWkvN5dGA04HK5FRKh6",
-  "1TE70kxS_x4jK1gEeUt77ldDX2GVIbTx9",
-  "1Ki1U2Oo9VKZ8kGdtOwAcGmyQJehh6AgJ",
-  "1dwt4hlMc4S-_ZJMVBNNbk5pNi8UeugtK",
-  "1zX_xXpD7KO8cySTITObbtjB651g2nQEx",
-  "1ccbT2CT_HCQTPDPfGCHOdP0SVLrmlRQb",
-];
-const instagram_users = [
-  "artur_rabelo",
-  "otavioosid",
-  "alexlagooliveira",
-  "scandolaragregory",
-  "sr_sagas",
-  "brenobmoreira",
-  "giicenci",
-  "gus.borgert",
-  "psachs",
-  "paulo_l_ferroli",
-  "Isaqueverona",
-  "savileonardo",
-  "duda_ingressos",
-  "eduardablm",
-  "fernanda_pilz",
-  "anabng",
-  "luizfdourado",
-  "mihmorim",
-  "seupromoteruniversitario",
-  "luanzera.02",
-  "marinunesdacosta",
-  "s.nathalia1",
-  "vi.menoncin",
-  "anandersenn",
-  "juterrivel",
-  "claraccardoso",
-  "irysschlickmann",
-];
-const numbers = [
-  "48991424482",
-  "48996961461",
-  "47984373660",
-  "48988216755",
-  "48996727451",
-  "48991031902",
-  "54996869057",
-  "48999444351",
-  "48996724493",
-  "48991110503",
-  "54996375677",
-  "48988193211",
-  "48991018103",
-  "48999627503",
-  "47996625085",
-  "48999880415",
-  "48999953766",
-  "11981003308",
-  "48996053430",
-  "11962524480",
-  "48988292998",
-  "48998677591",
-  "48999719662",
-  "48988048859",
-  "11943326176",
-  "48998342066",
-  "48996572288",
-];
+const sellers = [
+  {
+    "Name": "Marina Moro",
+    "phone": "(48) 999548425",
+    "insta": "mariinamoro",
+    "mail": "",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Arthur Heerdt",
+    "phone": "(48) 991277792",
+    "insta": "arthur.heerdt",
+    "mail": "",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Vitor Sagás",
+    "phone": "(48) 996727451",
+    "insta": "sr_sagas",
+    "mail": "vitorsagascustodio@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Breno Piniko",
+    "phone": "(48) 991031902",
+    "insta": "brenobmoreira",
+    "mail": "brenobmoreira20@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Gregory Selau",
+    "phone": "(48) 988216755",
+    "insta": "scandolaragregory",
+    "mail": "gregorysjs@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Gustavo Gonçalves",
+    "phone": "(48) 999444351",
+    "insta": "gus.borgert",
+    "mail": "gustavoborgertg@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Artur Rabelo",
+    "phone": "(48) 991424482",
+    "insta": "artur_rabelo",
+    "mail": "arturmrabelo@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Giulia Cenci",
+    "phone": "(54) 996869057",
+    "insta": "giicenci",
+    "mail": "giulia060202@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Thiago Comcap",
+    "phone": "(48) 991870716",
+    "insta": "thiagocunha1617",
+    "mail": "thiagocunhavs1@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Pedro Sachs",
+    "phone": "(48) 996724493",
+    "insta": "psachs",
+    "mail": "pedrosachsufsc@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Alex Lago",
+    "phone": "(47) 984373660",
+    "insta": "alexlagooliveira",
+    "mail": "alexlago2001@hotmail.com",
+    "Curso": "Autom, UFSC Blu"
+  },
+  {
+    "Name": "Artur Poeta",
+    "phone": "(48) 991805609",
+    "insta": "arturpoetaa_",
+    "mail": "artur.poeta27@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Karina Nathalie",
+    "phone": "(55) 991185541",
+    "insta": "karinanathaliebeck",
+    "mail": "karinanbeck@hotmail.com",
+    "Curso": "História, UFSC"
+  },
+  {
+    "Name": "Otávio Rafiri",
+    "phone": "(48) 996654393",
+    "insta": "otaviosjoaquim",
+    "mail": "otaviosilveira123@gmail.com",
+    "Curso": "Materiais, UFSC"
+  },
+  {
+    "Name": "Clara Cardoso",
+    "phone": "(48) 998342066",
+    "insta": "claraccardoso",
+    "mail": "clarinhah.cardoso@gmail.com",
+    "Curso": "AADUFSC, Direito, UFSC"
+  },
+  {
+    "Name": "Bruna Moraes",
+    "phone": "(91) 992423424",
+    "insta": "buhmoraes_",
+    "mail": "Bmoraes715@gmail.com",
+    "Curso": "Direito, Estácio"
+  },
+  {
+    "Name": "Duda Silva",
+    "phone": "(48) 991018103",
+    "insta": "duda_ingressos",
+    "mail": "ingressos.mariaeduardaas@gmail.com",
+    "Curso": "Psicologia, UNISUL"
+  },
+  {
+    "Name": "Luís Piva",
+    "phone": "(47) 991131611",
+    "insta": "luiispiva",
+    "mail": "luisgpiva@gmail.com",
+    "Curso": "Eletrônica, UFSC"
+  },
+  {
+    "Name": "Michelle Amorim",
+    "phone": "(11) 981003308",
+    "insta": "mihmorim",
+    "mail": "mihmorim@gmail.com",
+    "Curso": "Administração, UFSC"
+  },
+  {
+    "Name": "Maria Clara",
+    "phone": "(48) 991112002",
+    "insta": "marialimaa",
+    "mail": "mariacpl10@gmail.com",
+    "Curso": "Nutrição, UFSC"
+  },
+  {
+    "Name": "Duda Guimarães",
+    "phone": "(48) 998635452",
+    "insta": "dudagmr",
+    "mail": "maria.guimmaraes9@gmail.com",
+    "Curso": "Arquitetura, UNISUL"
+  },
+  {
+    "Name": "Bianca Souza",
+    "phone": "(48) 988136511",
+    "insta": "bianca.timm_",
+    "mail": "bianca.timm@outlook.com",
+    "Curso": "Economia, UFSC"
+  },
+  {
+    "Name": "Luan Taketa",
+    "phone": "(48) 988116425",
+    "insta": "luuan.g",
+    "mail": "luan.vtg@hotmail.com",
+    "Curso": "RI, UFSC"
+  },
+  {
+    "Name": "Vitória Menoncin",
+    "phone": "(48) 999719662",
+    "insta": "vi.menoncin",
+    "mail": "vitoriamenoncin@gmail.com",
+    "Curso": "Aquicultura, UFSC"
+  },
+  {
+    "Name": "Rafa Heider",
+    "phone": "(48) 991532300",
+    "insta": "rafa_heider",
+    "mail": "rafa.heider.rh@gmail.com",
+    "Curso": "Cavalaria, UNISUL PB"
+  }
+]
 
-const generateVendedor = (name, drive_images_id, instagram_user, number) => {
+const generateVendedor = (name, instagram_user, number, curso) => {
   return `
 <div id="t1-box">
     <main id="card">
         <div class="image-block">
-            <img src=https://drive.google.com/uc?export=view&id=${drive_images_id}>
+            <img src="fotos_vendedores/${name}.jpg">
         </div>
         <div class="text-block">
             <div class="header">
@@ -128,10 +189,15 @@ const generateVendedor = (name, drive_images_id, instagram_user, number) => {
             </div>
             <ul class="no-style">
                 <li>
-                    <a href="http://wa.me/55${number}" target="_blank"><i class="fab fa-whatsapp fa-2x" aria-hidden="true"></i></a>
-                </li>
+                    <a href="http://wa.me/55${number.replace(/\D/g, "")}" target="_blank"><i class="fab fa-whatsapp falg" aria-hidden="true"></i>${number}</a>
+                </li> 
+                <br>
                 <li>
-                    <a href="https://www.instagram.com/${instagram_user}/" target="_blank" color="blue"><i class="fab fa-instagram fa-2x" aria-hidden="true"></i></a>
+                    <a href="https://www.instagram.com/${instagram_user}/" target="_blank"><i class="fab fa-instagram falg" aria-hidden="true"></i>@${instagram_user}</a>
+                </li>
+                <br>
+                <li>
+                    <a><i class="fas fa-map-pin falg" aria-hidden="true"></i><b>${curso}</b></a>
                 </li>
             </ul>
         </div>
@@ -140,12 +206,7 @@ const generateVendedor = (name, drive_images_id, instagram_user, number) => {
 `;
 };
 
-for (let i = 0; i < names.length; i++) {
-  const vendedor = generateVendedor(
-    names[i],
-    drive_images_ids[i],
-    instagram_users[i],
-    numbers[i]
-  );
+for (let i = 0; i < sellers.length; i++) {
+  const vendedor = generateVendedor(sellers[i].Name, sellers[i].insta, sellers[i].phone, sellers[i].Curso);
   document.getElementById("t1-wrapper").innerHTML += vendedor;
 }

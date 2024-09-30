ce7ae5ac6aadeb0a93519ee5c684003930e04c12
tiago-hansen <tiagolorahansen@gmail.com>
2024-09-30 18:43:32 -0300

    Atualizar dados dos vendedores do 40

--- generateVendedores.js
+++ generateVendedores.js
@@ -3,14 +3,7 @@ const sellers = [
     "Name": "Marina Moro",
     "phone": "(48) 999548425",
     "insta": "mariinamoro",
-    "mail": "",
-    "Curso": "Automação, UFSC"
-  },
-  {
-    "Name": "Arthur Heerdt",
-    "phone": "(48) 991277792",
-    "insta": "arthur.heerdt",
-    "mail": "",
+    "mail": "marinamoro06@gmail.com",
     "Curso": "Automação, UFSC"
   },
   {
@@ -18,13 +11,6 @@ const sellers = [
     "phone": "(48) 996727451",
     "insta": "sr_sagas",
     "mail": "vitorsagascustodio@gmail.com",
-    "Curso": "Automação, UFSC"
-  },
-  {
-    "Name": "Breno Piniko",
-    "phone": "(48) 991031902",
-    "insta": "brenobmoreira",
-    "mail": "brenobmoreira20@gmail.com",
     "Curso": "Automação, UFSC"
   },
   {
@@ -49,20 +35,6 @@ const sellers = [
     "Curso": "Automação, UFSC"
   },
   {
-    "Name": "Giulia Cenci",
-    "phone": "(54) 996869057",
-    "insta": "giicenci",
-    "mail": "giulia060202@gmail.com",
-    "Curso": "Automação, UFSC"
-  },
-  {
-    "Name": "Thiago Comcap",
-    "phone": "(48) 991870716",
-    "insta": "thiagocunha1617",
-    "mail": "thiagocunhavs1@gmail.com",
-    "Curso": "Automação, UFSC"
-  },
-  {
     "Name": "Pedro Sachs",
     "phone": "(48) 996724493",
     "insta": "psachs",
@@ -77,53 +49,11 @@ const sellers = [
     "Curso": "Autom, UFSC Blu"
   },
   {
-    "Name": "Artur Poeta",
-    "phone": "(48) 991805609",
-    "insta": "arturpoetaa_",
-    "mail": "artur.poeta27@gmail.com",
-    "Curso": "Automação, UFSC"
-  },
-  {
-    "Name": "Karina Nathalie",
-    "phone": "(55) 991185541",
-    "insta": "karinanathaliebeck",
-    "mail": "karinanbeck@hotmail.com",
-    "Curso": "História, UFSC"
-  },
-  {
-    "Name": "Otávio Rafiri",
-    "phone": "(48) 996654393",
-    "insta": "otaviosjoaquim",
-    "mail": "otaviosilveira123@gmail.com",
-    "Curso": "Materiais, UFSC"
-  },
-  {
     "Name": "Clara Cardoso",
     "phone": "(48) 998342066",
     "insta": "claraccardoso",
     "mail": "clarinhah.cardoso@gmail.com",
     "Curso": "AADUFSC, Direito, UFSC"
-  },
-  {
-    "Name": "Bruna Moraes",
-    "phone": "(91) 992423424",
-    "insta": "buhmoraes_",
-    "mail": "Bmoraes715@gmail.com",
-    "Curso": "Direito, Estácio"
-  },
-  {
-    "Name": "Duda Silva",
-    "phone": "(48) 991018103",
-    "insta": "duda_ingressos",
-    "mail": "ingressos.mariaeduardaas@gmail.com",
-    "Curso": "Psicologia, UNISUL"
-  },
-  {
-    "Name": "Luís Piva",
-    "phone": "(47) 991131611",
-    "insta": "luiispiva",
-    "mail": "luisgpiva@gmail.com",
-    "Curso": "Eletrônica, UFSC"
   },
   {
     "Name": "Michelle Amorim",
@@ -133,32 +63,11 @@ const sellers = [
     "Curso": "Administração, UFSC"
   },
   {
-    "Name": "Maria Clara",
-    "phone": "(48) 991112002",
-    "insta": "marialimaa",
-    "mail": "mariacpl10@gmail.com",
-    "Curso": "Nutrição, UFSC"
-  },
-  {
-    "Name": "Duda Guimarães",
-    "phone": "(48) 998635452",
-    "insta": "dudagmr",
-    "mail": "maria.guimmaraes9@gmail.com",
-    "Curso": "Arquitetura, UNISUL"
-  },
-  {
-    "Name": "Bianca Souza",
+    "Name": "Bianca Timm",
     "phone": "(48) 988136511",
     "insta": "bianca.timm_",
     "mail": "bianca.timm@outlook.com",
     "Curso": "Economia, UFSC"
-  },
-  {
-    "Name": "Luan Taketa",
-    "phone": "(48) 988116425",
-    "insta": "luuan.g",
-    "mail": "luan.vtg@hotmail.com",
-    "Curso": "RI, UFSC"
   },
   {
     "Name": "Vitória Menoncin",
@@ -168,11 +77,137 @@ const sellers = [
     "Curso": "Aquicultura, UFSC"
   },
   {
-    "Name": "Rafa Heider",
-    "phone": "(48) 991532300",
-    "insta": "rafa_heider",
-    "mail": "rafa.heider.rh@gmail.com",
-    "Curso": "Cavalaria, UNISUL PB"
+    "Name": "Ana Cella",
+    "phone": "(48) 991433455",
+    "insta": "anacella",
+    "mail": "ccella.anac@gmail.com",
+    "Curso": "Aquicultura, UFSC"
+  },
+  {
+    "Name": "Arthur Medeiros",
+    "phone": "(48) 991017601",
+    "insta": "medeiro_arthur",
+    "mail": "arthurmm2004@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Arthur Pinheiro",
+    "phone": "(48) 996388345",
+    "insta": "arthpinheiro",
+    "mail": "arthurpinheiro9@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Bibiane Tonelero",
+    "phone": "(54) 991772277",
+    "insta": "bibitonelero",
+    "mail": "bibianetonelero@gmail.com",
+    "Curso": "Biologia, UFSC"
+  },
+  {
+    "Name": "Diego Avaiano",
+    "phone": "(48) 999912513",
+    "insta": "diavaiano",
+    "mail": "eusou@avaiano.com.br",
+    "Curso": "Administração, UFSC"
+  },
+  {
+    "Name": "Gabriel Alves",
+    "phone": "(48) 996727794",
+    "insta": "gabriel_msalves",
+    "mail": "gabrielmsalves@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Gabriele Nicolli",
+    "phone": "(45) 991480505",
+    "insta": "gabnicolli",
+    "mail": "gabrielenicolli@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Gisele Alves",
+    "phone": "(47) 992143724",
+    "insta": "gisa_eu",
+    "mail": "alves.gisele11@gmail.com",
+    "Curso": "Produção, FURB"
+  },
+  {
+    "Name": "Gustavo Martins",
+    "phone": "(48) 998395206",
+    "insta": "martinsgu._",
+    "mail": "gustavomf0606@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Isaque Verona",
+    "phone": "(54) 996375677",
+    "insta": "Isaqueverona ",
+    "mail": "isaque.verona@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Julio Monteiro",
+    "phone": "(12) 996700204",
+    "insta": "juliosmonteiro",
+    "mail": "juliosmonteiro78@gmail.com",
+    "Curso": "Contábeis, UFSC"
+  },
+  {
+    "Name": "Krysleine (Dindossauros)",
+    "phone": "(48) 984366785",
+    "insta": "Dindossauros",
+    "mail": "krys.g@hotmail.com",
+    "Curso": "Administração, IES"
+  },
+  {
+    "Name": "Luan de Almeida",
+    "phone": "(11) 962524480",
+    "insta": "luanzera.02",
+    "mail": "luanalmeidasccp@gmail.com",
+    "Curso": "Engenharia Química, UFSC"
+  },
+  {
+    "Name": "Marcos Abreu",
+    "phone": "(48) 984599937",
+    "insta": "marcos_asantosjr",
+    "mail": "marcosabreudosantos@gmail.com",
+    "Curso": "Computação, Estácio"
+  },
+  {
+    "Name": "Maria Eduarda Guimarães",
+    "phone": "(48) 998635452",
+    "insta": "dudagmr",
+    "mail": "maria.guimmaraes9@gmail.com",
+    "Curso": "Arquitetura, Unisul"
+  },
+  {
+    "Name": "Mariana Hikari",
+    "phone": "(12) 991853687",
+    "insta": "marianahikari",
+    "mail": "marianahikarimata@gmail.com",
+    "Curso": "Produção Multimídia, IFSC"
+  },
+  {
+    "Name": "Mirella Fernanda Nunes",
+    "phone": "(47) 992749782",
+    "insta": "mf_nunes",
+    "mail": "mirellafernandanunes@gmail.com",
+    "Curso": "Automação, UFSC Blu"
+  },
+  {
+    "Name": "Paulo Ferroli",
+    "phone": "(48) 991110503",
+    "insta": "paulo_l_ferroli",
+    "mail": "ferrolipaulolibrelotto@gmail.com",
+    "Curso": "Automação, UFSC"
+  },
+  {
+    "Name": "Rafael Bernard",
+    "phone": "(48) 991235900",
+    "insta": "rafaelbernard_",
+    "mail": "rafaelpbernard@gmail.com",
+    "Curso": "Engenharia Civil, UFSC"
   }
 ]
 
